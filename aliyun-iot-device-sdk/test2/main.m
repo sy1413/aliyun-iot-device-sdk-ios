a603c30fc7941bdfc268eb948d0a2228ded4a841
sy1413 <sy1413@163.com>
2017-01-05 20:40:56 +0800

    阿里iot集成

---
+++ aliyun-iot-device-sdk/test2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  test2
+//
+//  Created by 11 on 17/1/5.
+//  Copyright © 2017年 11. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
